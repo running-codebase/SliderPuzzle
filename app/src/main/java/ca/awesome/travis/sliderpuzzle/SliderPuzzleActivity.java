f2643fe7223e392523efaf2d79569827a56ebbc3
Travis Coulter <tco@Traviss-MBP.gateway>
2016-04-13 08:38:26 +1000

    Initial commit of new project

---
+++ app/src/main/java/ca/awesome/travis/sliderpuzzle/SliderPuzzleActivity.java
@@ -0,0 +1,13 @@
+package ca.awesome.travis.sliderpuzzle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SliderPuzzleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slider_puzzle);
+    }
+}
