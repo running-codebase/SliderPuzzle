6c7233df0cba41c09059c1e046cac81d310196ed
running-codebase <info@traviscoulter.ca>
2016-04-14 11:46:53 +1000

    Updated PuzzleView. Effort was taken to remove all code that was seperate from creating the tiles, accepting and tracking touch movement and updating tile positions.

--- app/src/main/java/ca/awesome/travis/sliderpuzzle/PuzzleView.java
+++ app/src/main/java/ca/awesome/travis/sliderpuzzle/PuzzleView.java
@@ -3,32 +3,38 @@ package ca.awesome.travis.sliderpuzzle;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by tco on 16-04-13.
  */
 public class PuzzleView extends RelativeLayout implements View.OnTouchListener {
 
-    private static final float BACKGROUND_ALPHA = 0.2f;
     private static final int ROWS = 4;
     private static final int COLUMNS = 4;
-
+    private static final int EMPTY_TILE_ID = -1;
 
     private Context context;
-    private ImageView backgroundImageView;
     private boolean viewInitialized = false;
 
     private ArrayList<TilePieceView> tilePieceViews = new ArrayList<>();
+    private ArrayList<TilePieceView> movingTilePieceViews = new ArrayList<>();
+    private GridCalculations.Direction movingDirection = GridCalculations.Direction.NONE;
+
+    private TilePieceView emptyTilePieceView;
+    private TilePieceView selectedTilePieceView;
+
+    private GridCalculations gridCalc;
+    private int tileWidth, tileHight;
+    private float initialTouchX, initialTouchY;
+
 
     public PuzzleView(Context context) {
         super(context);
@@ -45,39 +51,37 @@ public class PuzzleView extends RelativeLayout implements View.OnTouchListener {
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
         if (!viewInitialized) {
-//            initializeBackground();
-            initializeTiles();
+            tileWidth = getWidth() / COLUMNS;
+            tileHight = getWidth() / ROWS;
+            gridCalc = new GridCalculations(tileWidth, tileHight);
 
+            initializeTiles();
             viewInitialized = true;
         }
     }
 
-    private void initializeBackground() {
-
-        backgroundImageView = new ImageView(context);
-        backgroundImageView.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.globe));
-        backgroundImageView.setAlpha(BACKGROUND_ALPHA);
-
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(getWidth(), getHeight());
-        params.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
-        addView(backgroundImageView, params);
-    }
-
     private void initializeTiles() {
+        //Initialize images, remove bottom right corner and shuffle
         ArrayList<Bitmap> puzzleTiles = ImageSlicer.sliceImage(ContextCompat.getDrawable(context, R.drawable.globe), ROWS, COLUMNS);
+        puzzleTiles.remove(puzzleTiles.size() - 1);
+
+        int currentId = 0;
         TilePieceView tilePiece;
         for (int row = 0; row < ROWS; row ++) {
             for (int column = 0; column < COLUMNS; column++) {
-//                IF this is the bottom right tilePiece leave it empty
+//              If this is the bottom right tilePiece leave it empty
                 if (row == ROWS -1 && column == COLUMNS -1) {
-                    tilePiece = new TilePieceView(context, column, row, true);
+                    tilePiece = new TilePieceView(context, currentId, column, row, true);
+                    emptyTilePieceView = tilePiece;
                 } else {
-                    tilePiece = new TilePieceView(context, column, row, false);
+                    tilePiece = new TilePieceView(context, currentId, column, row, false);
                     tilePiece.setImageBitmap(puzzleTiles.get(0));
                     placeTilePiece(tilePiece);
+
+                    puzzleTiles.remove(0);
                 }
                 tilePieceViews.add(tilePiece);
-                puzzleTiles.remove(0);
+                currentId++;
             }
         }
     }
@@ -96,23 +100,50 @@ public class PuzzleView extends RelativeLayout implements View.OnTouchListener {
         params.leftMargin = containerRect.left;
         addView(tilePiece, params);
         tilePiece.setOnTouchListener(this);
+        tilePiece.setHomeX(left);
+        tilePiece.setHomeY(top);
 
     }
 
 
-
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        TilePieceView tilePiece = (TilePieceView) v;
-        if (!tilePiece.isEmpty()) {
+        TilePieceView touchedTilePiece = (TilePieceView) v;
+        if (!touchedTilePiece.isEmpty() && gridCalc.tilePieceInSameRowOrColumnToEmptyTile(touchedTilePiece, emptyTilePieceView)) {
+
+            selectedTilePieceView = touchedTilePiece;
             switch (event.getActionMasked()){
                 case MotionEvent.ACTION_DOWN:
-                    tilePiece.setAlpha(0.1f);
+                    movingTilePieceViews.clear();
+
+                    initialTouchX = event.getRawX();
+                    initialTouchY = event.getRawY();
+                    movingTilePieceViews = gridCalc.tilesPiecesThatMove(touchedTilePiece, emptyTilePieceView, tilePieceViews);
+                    movingDirection = gridCalc.calculateDirection(touchedTilePiece, emptyTilePieceView);
                     break;
-                case MotionEvent.ACTION_UP:
-                    tilePiece.setAlpha(1f);
-                    break;
+
                 case MotionEvent.ACTION_MOVE:
+                    float deltaX = event.getRawX() - initialTouchX;
+                    float deltaY = event.getRawY() - initialTouchY;
+                    float[] constrainedTemporaryDeltas = gridCalc.constrainTemporaryMovement(movingDirection, deltaX, deltaY);
+                    temporarilyMoveTiles(constrainedTemporaryDeltas[0], constrainedTemporaryDeltas[1]);
+                    break;
+
+                case MotionEvent.ACTION_UP:
+                    float finalDeltaX = event.getRawX() - initialTouchX;
+                    float finalDeltaY = event.getRawY() - initialTouchY;
+                    float[] constrainedFinalDeltas = gridCalc.constrainTemporaryMovement(movingDirection, finalDeltaX, finalDeltaY);
+
+                    float[] finalDeltas = gridCalc.snapFinalMovement(movingDirection, constrainedFinalDeltas[0], constrainedFinalDeltas[1]);
+                    if (GridCalculations.movementTriggeredTileUpdate(finalDeltas[0], finalDeltas[1])) {
+                        moveTilesToFinalPosition(finalDeltas[0], finalDeltas[1]);
+                        emptyTilePieceView = new TilePieceView(context, EMPTY_TILE_ID, selectedTilePieceView.getRow(), selectedTilePieceView.getColumn(), true);
+                        updateTilePieceViewsRowAndColumn(finalDeltas[0], finalDeltas[1]);
+                    } else {
+                        moveTilesToFinalPosition(finalDeltas[0], finalDeltas[1]);
+                    }
+
+                    movingDirection = GridCalculations.Direction.NONE;
                     break;
             }
         }
@@ -120,4 +151,37 @@ public class PuzzleView extends RelativeLayout implements View.OnTouchListener {
     }
 
 
+    public void temporarilyMoveTiles(float deltaX, float deltaY) {
+        for (TilePieceView tilePieceView: movingTilePieceViews){
+            tilePieceView.setX(tilePieceView.getHomeX() + deltaX);
+            tilePieceView.setY(tilePieceView.getHomeY() + deltaY);
+        }
+    }
+
+    public void moveTilesToFinalPosition(float deltaX, float deltaY) {
+
+        for (TilePieceView tilePieceView: movingTilePieceViews){
+            tilePieceView.setX(tilePieceView.getHomeX() + deltaX);
+            tilePieceView.setY(tilePieceView.getHomeY() + deltaY);
+            tilePieceView.setHomeX(tilePieceView.getHomeX() + (int) deltaX);
+            tilePieceView.setHomeY(tilePieceView.getHomeY() + (int) deltaY);
+        }
+    }
+
+    public void updateTilePieceViewsRowAndColumn(float deltaX, float deltaY) {
+
+        for (TilePieceView tilePieceView: movingTilePieceViews){
+            if (deltaX < 0 ) {
+                tilePieceView.setColumn(tilePieceView.getColumn() - 1);
+            } else if (deltaX > 0 ) {
+                tilePieceView.setColumn(tilePieceView.getColumn() + 1);
+            }
+
+            if (deltaY < 0 ) {
+                tilePieceView.setRow(tilePieceView.getRow() - 1);
+            } else if (deltaY > 0 ){
+                tilePieceView.setRow(tilePieceView.getRow() + 1);
+            }
+        }
+    }
 }
